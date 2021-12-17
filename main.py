aad26744c86b4dcf27ebc8d19ef748fbe5e272bf
islam megdoude <salaheddine@univ-msila.dz>
2021-12-18 00:23:18 +0100

    damdam

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import turtle
 
 wind = turtle.Screen()
-wind.title("Pong by Hesham jesse")
+wind.title("Pong by ElMegdoude")
 wind.bgcolor("black")
 wind.setup(width=800, height=600)
 wind.tracer(0)
@@ -25,29 +25,50 @@ hatba2.penup()
 hatba2.goto(350, 0)
 # ball
 ball = turtle.Turtle()
-ball.speed()
+ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-#functions
+ball.dx = 0.4
+ball.dy = 0.4
+#score
+score1 = 0
+score2 = 0
+score = turtle.Turtle()
+score.speed(0)
+score.color("white")
+score.penup()
+score.hideturtle()
+score.goto(0, 260)
+
+
+# functions
 def hatba1_up():
     y = hatba1.ycor()
     y += 20
     hatba1.sety(y)
+
+
 def hatba1_down():
     y = hatba1.ycor()
     y -= 20
     hatba1.sety(y)
+
+
 def hatba2_up():
     y = hatba2.ycor()
     y += 20
     hatba2.sety(y)
+
+
 def hatba2_down():
     y = hatba2.ycor()
     y -= 20
     hatba2.sety(y)
-#keyboard bindings
+
+
+# keyboard bindings
 wind.listen()
 wind.onkeypress(hatba1_up, "z")
 wind.onkeypress(hatba1_down, "d")
@@ -56,3 +77,34 @@ wind.onkeypress(hatba2_down, "Down")
 # main game loop
 while True:
     wind.update()
+
+    # move th ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # border check
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score1 += 1
+        score.clear()
+        score.write("Palyer 1 : {} **** Player 2 : {}".format(score1, score2), align="center", font=("Courier", 22, "normal"))
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score2 += 1
+        score.clear()
+        score.write("Palyer 1 : {} **** Player 2 : {}".format(score1, score2), align="center", font=("Courier", 22, "normal"))
+
+    if (ball.xcor()>340 and ball.xcor()<350) and (ball.ycor()<hatba2.ycor()+40 and ball.ycor()>hatba2.ycor()-40):
+        ball.setx(340)
+        ball.dx *= -1
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < hatba1.ycor() + 40 and ball.ycor() > hatba1.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
